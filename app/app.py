b2c01ed318f4efca5bd6c562ff120b53df599a83
中川　悠 <y.nakagawa@a1707-039.opt-sys.local>
2019-09-18 17:36:07 +0900

    to day5

--- app/app.py
+++ app/app.py
@@ -1,5 +1,10 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
 from flask import Flask,render_template,request
+from models.models import KitsuneContest
+
+from models.database import db_session
+from datetime import datetime
+
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
@@ -15,14 +20,43 @@ def hello():
 @app.route("/index")
 def index():
     name = request.args.get("name")
-    song = ["こぶた","たぬき","きつね🦊","ねこ"]
-    return render_template("index.html",name=name,song=song)
+    #song = ["こぶた","たぬき","きつね🦊","ねこ"]
+
+    all_contests = KitsuneContest.query.all()
+    return render_template("index.html", name=name, all_contests=all_contests)
 
 @app.route("/index",methods=["post"])
 def post():
     name = request.form["name"]
-    song = ["こぶた", "たぬき", "きつね🦊", "ねこ"]
-    return render_template("index.html", name=name, song=song)
+    #song = ["こぶた", "たぬき", "きつね🦊", "ねこ"]
+    all_contests = KitsuneContest.query.all()
+    return render_template("index.html", name=name, all_contests=all_contests)
+
+@app.route("/add",methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = KitsuneContest(title,body,datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return index()
+
+@app.route("/update",methods=["post"])
+def update():
+    content = KitsuneContest.query.filter_by(id=request.form["update"]).first()
+    content.title = request.form["title"]
+    content.body = request.form["body"]
+    db_session.commit()
+    return index()
+
+@app.route("/delete",methods=["post"])
+def delete():
+    id_list = request.form.getlist("delete")
+    for id in id_list:
+        content = KitsuneContest.query.filter_by(id=id).first()
+        db_session.delete(content)
+    db_session.commit()
+    return index()
 
 #おまじない
 if __name__ == "__main__":
--- models/models.py
+++ models/models.py
@@ -3,8 +3,8 @@ from models.database import Base
 from datetime import datetime
 
 
-class OnegaiContent(Base):
-    __tablename__ = 'onegaicontents'
+class KitsuneContest(Base):
+    __tablename__ = 'kitsune'
     id = Column(Integer, primary_key=True)
     title = Column(String(128), unique=True)
     body = Column(Text)
